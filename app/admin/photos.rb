079a36576db3911b6a7188edc7b218fa208937fa
Mycroft1891 <mycroft1891@gmail.com>
2018-02-14 04:09:00 +0100

    change active_admin photo index and show

--- app/admin/photos.rb
+++ app/admin/photos.rb
@@ -2,6 +2,25 @@ ActiveAdmin.register Photo do
 
   permit_params :description, :image
 
+  index do
+    id_column
+    column :description
+    column :image
+    column :preview do |photo|
+      image_tag(photo.image.url, width: 300)
+    end
+    actions
+  end
+
+  show do |t|
+    attributes_table do
+      row :description
+      row :image do |photo|
+        image_tag(photo.image.url, width: 500 )
+      end
+    end
+  end
+
   form do |f|
     f.inputs do
       f.input :description
