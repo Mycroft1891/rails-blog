5bf9b74d873b913341a431d63dddd4e80cf52553
Mycroft1891 <mycroft1891@gmail.com>
2018-02-16 01:55:02 +0100

    add tags to active admin and styling

--- app/admin/posts.rb
+++ app/admin/posts.rb
@@ -1,10 +1,11 @@
 ActiveAdmin.register Post do
-  permit_params :title, :body, :permalink
+  permit_params :title, :body, :permalink, :tag_list
 
   form do |f|
     f.inputs do
       f.input :title
       f.input :body, as: :medium_editor, input_html: { data: { options: '{"spellcheck":true,"toolbar":{"buttons":["bold","italic","underline","anchor","orderedlist","unorderedlist","strikethrough","subscript","superscript","pre","h1","h2","h3","h4","h5","h6","html"]}}' } }
+      f.input :tag_list
       f.input :permalink
     end
     f.actions
