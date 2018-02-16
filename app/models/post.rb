1165f4dcfea0eaa237fe6515492e6efa2dd87a66
Mycroft1891 <mycroft1891@gmail.com>
2018-02-16 01:50:18 +0100

    add tags to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,16 @@
 class Post < ApplicationRecord
+  has_many :taggings
+  has_many :tags, through: :taggings
 
   default_scope lambda { order(created_at: :desc) }
+
+  def tag_list=(list)
+    tag_list = list.split(',').collect { |t| t.strip.downcase }.uniq
+    new_and_found_tags = tag_list.collect { |t| Tag.find_or_create_by(name: t) }
+    self.tags = new_and_found_tags
+  end
+
+  def tag_list
+    self.tags.collect(&:name).join(', ')
+  end
 end
